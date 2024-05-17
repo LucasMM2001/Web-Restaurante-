9a6828f0039b538229cd28092a69f7d7b61ce789
LucasMM2001 <lucasmiady@gmail.com>
2024-05-16 21:56:03 -0300

    Primeiro Commit

---
+++ web-api-restaurante/web-api-restaurante/Entidades/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace web_api_restaurante.Entidades
+{
+    public class Produto
+    {
+        public int id { get; set; }
+
+        public string nome { get; set; }    
+
+        public string descricao { get; set;}
+
+        public string ImagemUrl { get; set; }
+
+
+
+
+
+
+    }
+}
